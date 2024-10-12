3fc75b23707313281665b0c0ac728bc9e0235ae4
JSilvis <SilvisDevelopment@gmail.com>
2024-10-12 18:27:15 -0400

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className=''>
+        <h1 className='text-xl text-teal-300'>Psychronize</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
