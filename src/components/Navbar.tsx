dfb10d00e52b124955548da8444dc58b2a2fec02
JSilvis <SilvisDevelopment@gmail.com>
2024-10-13 19:42:54 -0400

    Created UserProfile and continueing layout.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ export default function Navbar() {
   return (
     <nav className='absolute w-full top-0 border-2 border-yellow-400 flex flex-row justify-center'>
         <ul className='flex flex-row  gap-4'>
-            <li>ONE</li>
-            <li>TWO</li>
-            <li>THREE</li>
+            <li><a>DashBoard</a> </li>
+            <li>Accounts</li>
+            <li>Operations</li>
         </ul>
     </nav>
   )
