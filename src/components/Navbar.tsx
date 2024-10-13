25e80aa012e4aea97a1273e5937360a038d42bfc
JSilvis <SilvisDevelopment@gmail.com>
2024-10-13 19:54:20 -0400

    Working on navbar layout.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,17 @@ import Button from './primitive/Button'
 
 export default function Navbar() {
   return (
-    <nav className='absolute w-full top-0 border-2 border-yellow-400 flex flex-row justify-center'>
-        <ul className='flex flex-row  gap-4'>
-            <li><a>DashBoard</a> </li>
+    <nav className='absolute w-full top-0 border-2 border-yellow-400 flex flex-row'>
+  
+             <p className='flex-start'>logo</p>
+           
+        <ul className='border-2 flex flex-row  gap-4'>
+            <li><a>DashBoard</a></li>
             <li>Accounts</li>
             <li>Operations</li>
         </ul>
+      
+       
     </nav>
   )
 }
